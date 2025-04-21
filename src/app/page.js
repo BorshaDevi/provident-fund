e06df560e4586f54145ccc6e21178c9b686f9b77
unknown <borshadevi27@gmail.com>
2025-04-21 15:49:08 +0600

    added EmployeeList compent

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import EmployeeList from "@/compent/EmployeeList";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="">Hello world!</h1>
+      <EmployeeList></EmployeeList>
     </div>
   );
 }
