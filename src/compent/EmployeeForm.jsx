73869dec62981ab3f9109e005f878c58f0b557d3
unknown <borshadevi27@gmail.com>
2025-04-21 14:09:54 +0600

    install calender ,popover

--- src/compent/EmployeeForm.jsx
+++ src/compent/EmployeeForm.jsx
@@ -11,6 +11,7 @@ import {
     FormMessage,
   } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 const formSchema = z.object({
     firstName: z.string().min(2 ,{
         message:"Name must be at least 2 characters long"
@@ -66,6 +67,7 @@ const EmployeeForm= ()=>{
         <div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
+                    {/* name */}
                 <div className="grid grid-cols-2 gap-5">
                 <FormField
                 control={form.control}
@@ -94,13 +96,54 @@ const EmployeeForm= ()=>{
                 )}
                 />
                 </div>
+
+                {/* gender , designation  */}
                 <div className="grid grid-cols-2 gap-5 mt-5">
                 <FormField
                 control={form.control}
-                name="firstName"
+                name="gender"
+                
                 render={({field})=>(
                   <FormItem >
-                    <FormLabel>Name</FormLabel>
+                    <FormLabel>Gender</FormLabel>
+                    <FormControl>
+                      <Select {...field} onValueChange={field.onChange} defaultValue={field.value} placeholder='Select' >
+                        <SelectTrigger className="w-[180px] ">
+                            <SelectValue placeholder ='Select'></SelectValue>
+                        </SelectTrigger>
+                          <SelectContent className='bg-white'>
+                            <SelectItem value='female'>Female</SelectItem>
+                            <SelectItem value='male'>male</SelectItem>
+                          </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+                />
+                <FormField
+                control={form.control}
+                name="designation"
+                render={({field})=>(
+                  <FormItem >
+                    <FormLabel>Designation </FormLabel>
+                    <FormControl>
+                      <input type='text' {...field} placeholder="designation" className='border-2 border-black p-1 rounded-md' />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+                />
+                </div>
+
+                {/* employeeId , date of birth */}
+                <div className="grid grid-cols-2 gap-5 mt-5">
+                <FormField
+                control={form.control}
+                name="employeeId"
+                render={({field})=>(
+                  <FormItem >
+                    <FormLabel>Employee Id</FormLabel>
                     <FormControl>
                       <input type='text' {...field} placeholder="First Name" className='border-2 border-black p-1 rounded-md' />
                     </FormControl>
@@ -110,10 +153,10 @@ const EmployeeForm= ()=>{
                 />
                 <FormField
                 control={form.control}
-                name="lastName"
+                name="dateOfBirth"
                 render={({field})=>(
                   <FormItem >
-                    <FormLabel>Last Name</FormLabel>
+                    <FormLabel>Date of Birth</FormLabel>
                     <FormControl>
                       <input type='text' {...field} placeholder="Last Name" className='border-2 border-black p-1 rounded-md' />
                     </FormControl>
