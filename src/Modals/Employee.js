42a6165a47f24d202e67242f8d01bb113b5bc4ca
unknown <borshadevi27@gmail.com>
2025-04-21 13:09:08 +0600

    added Employee.js modal

--- src/Modals/Employee.js
+++ src/Modals/Employee.js
@@ -10,6 +10,38 @@ const employeeSchema = new mongoose.Schema({
         required:true,
         uppercase:true,
     },
+    employeeId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    employeeEmail:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+    gender:{
+        type:String,
+        required:true,
+    },
+    dateOfBirth:{
+        type:Date,
+        required:true,
+    },
+    dateOfConfirmation:{
+        type:Date,
+        required:true,
+    },
+    designation:{
+        type:String,
+        required:true,
+    },
+    basicSalary:{
+        type:Number,
+        required:true,
+    },
+
 })
-const Employee= mongoose.model('employee',employeeSchema);
-export default Employee || mongoose.models.Employee;
+const Employee= mongoose.models.employee || mongoose.model('employee',employeeSchema);
+export default Employee ;
